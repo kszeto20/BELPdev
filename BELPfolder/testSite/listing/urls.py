ec5d46200f5c6106b65cc362d09ef9c630897aa4
Lillian Tran <lilliantran@lillians-air.78m475.nycboe.org>
2022-05-23 17:04:41 -0400

    bathroom_list.html is being looked for in listing/listing which does not exist

--- BELPfolder/testSite/listing/urls.py
+++ BELPfolder/testSite/listing/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/listing', views.BathroomListView.as_view(), name='bathroom')
+    path('listing', views.BathroomListView.as_view(), name='bathroom')
 ]
